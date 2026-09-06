e57fd56125e52dd862a8c522b45a7977a9a41638
abbe1604vp-ops <abbe1604.vp@gmail.com>
2026-09-06 13:00:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 # --- CONFIGURACIÓN ---
-TELEGRAM_BOT_TOKEN = "6327813571:AAGcRK1xNEVy9xqC2SHqQxZiK7I1sOzq89I"
+TELEGRAM_BOT_TOKEN = "6327813571:AAEyxTi8Uun0sKuOKSgUfWjz69-6uTrnewM"
 CHAT_ID_NOTIFICACIONES = None
 
 FIAT = "VES"
